f3ab75a902e0a7b2768d6fccc9962fd286f0afee
MrLarkyy_ <studiotadikl@gmail.com>
2026-01-08 01:10:16 +0100

    Add Maven publishing configuration with .env example and plugin adjustments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
+    `maven-publish`
 }
 
 group = "gg.aquatic.kommand"
@@ -31,4 +33,34 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
+publishing {
+    repositories {
+        maven {
+            name = "aquaticRepository"
+            url = uri("https://repo.nekroplex.com/releases")
+
+            credentials {
+                username = maven_username
+                password = maven_password
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.aquatic"
+            artifactId = "KRegistry"
+            version = "${project.version}"
+
+            from(components["java"])
+            //artifact(tasks.compileJava)
+        }
+    }
 }
